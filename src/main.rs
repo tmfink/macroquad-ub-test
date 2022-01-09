f092e731047785453d406b58dd8b5f75582925e9
Travis Finkenauer <tmfinken@gmail.com>
2022-01-08 17:18:02 -0800

    Try with RacyCell<MaybeUninit<Context>>
    Does not gracefully fail

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 //! Simulates `get_internal_gl()` in macroquad
 
+use std::{cell::UnsafeCell, marker::PhantomData, mem::MaybeUninit};
+
 /******************** lib.rs **********************/
 
+// from https://github.com/rust-lang/rust/issues/53639#issuecomment-790091647
+struct RacyCell<T>(UnsafeCell<T>);
+
+impl<T> RacyCell<T> {
+    const fn new(value: T) -> Self {
+        RacyCell(UnsafeCell::new(value))
+    }
+
+    unsafe fn get_mut_unchecked(&self) -> &mut T {
+        &mut *self.0.get()
+    }
+}
+
+unsafe impl<T> Sync for RacyCell<T> {}
+
 #[no_mangle]
-static mut CONTEXT: Option<Context> = None;
+//static mut CONTEXT: Option<Context> = None;
+static CONTEXT: RacyCell<MaybeUninit<Context>> = RacyCell::new(MaybeUninit::uninit());
 
 struct Context {
     quad_context: u32,
     gl: u32,
 }
 
-fn get_context() -> &'static mut Context {
-    unsafe { CONTEXT.as_mut().unwrap_or_else(|| panic!()) }
+unsafe fn init_context() {
+    *CONTEXT.get_mut_unchecked() = MaybeUninit::new(Context {
+        quad_context: 0,
+        gl: 0,
+    })
+}
+
+/// # SAFETY
+/// Requirements:
+/// - `init_context()` must be called before this function.
+/// - this function must only be called from the "main" thread
+unsafe fn get_context() -> *mut Context {
+    CONTEXT.get_mut_unchecked().assume_init_mut()
 }
 
 impl Context {
@@ -23,13 +52,16 @@ impl Context {
 
 /******************** window.rs **********************/
 pub struct InternalGlContext<'a> {
-    pub quad_context: &'a mut u32,
-    pub quad_gl: &'a mut u32,
+    pub quad_context: *mut u32,
+    pub quad_gl: *mut u32,
+    _phantom: PhantomData<&'a ()>,
 }
 
 impl<'a> InternalGlContext<'a> {
     pub fn flush(&mut self) {
-        get_context().perform_render_passes();
+        unsafe {
+            (*get_context()).perform_render_passes();
+        }
     }
 }
 
@@ -37,25 +69,37 @@ pub unsafe fn get_internal_gl<'a>() -> InternalGlContext<'a> {
     let context = get_context();
 
     InternalGlContext {
-        quad_context: &mut context.quad_context,
-        quad_gl: &mut context.gl,
+        quad_context: &mut (*context).quad_context as *mut u32,
+        quad_gl: &mut (*context).gl as *mut u32,
+        _phantom: Default::default(),
     }
 }
 
 /******************** use of lib **********************/
+
+fn helper() {
+    unsafe {
+        let mut gl = get_internal_gl();
+        gl.flush();
+        *gl.quad_context *= 2;
+    }
+}
+
 fn main() {
     // Simulates Window::from_config()
     unsafe {
-        CONTEXT = Some(Context {
-            quad_context: 0,
-            gl: 0,
-        })
-    };
+        // must be called before `get_context()`
+        init_context();
+    }
 
     // Simulate game loop
     {
         let mut gl = unsafe { get_internal_gl() };
         gl.flush();
-        *gl.quad_context += 1; // miri complains here
+        unsafe {
+            *gl.quad_context += 1;
+        }
+        helper();
+        gl.flush();
     }
 }
